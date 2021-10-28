84fcf2c9bf1dab6f9d4a19fc6b558e85d4e1c989
Haotian Wu <3233990645@qq.com>
2021-10-28 15:58:25 +0800

    Update get_asins.py

--- get_asins.py
+++ get_asins.py
@@ -1,13 +1,20 @@
-import csv
 import json
 import pandas as pd
 
 f = open("E:\\Movies_and_TV_5\\Movies_and_TV_5.json")
 js = []
+formerAsin = "null"
 for line in f.readlines():
     js_l = json.loads(line)
+
+    # 简单去重
+    Asin = js_l['asin']
+    if Asin == formerAsin:
+        continue
+    formerAsin = Asin
+
     js.append(js_l['asin'])
     print(js_l['asin'])
 test = pd.DataFrame(columns=['asin'], data=js).drop_duplicates()
 test.to_csv('asin.csv')
-print(js)
+# print(js)
