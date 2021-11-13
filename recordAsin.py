4a6761dfef39cfa3bb993df46a9c92862647b9da
Haotian Wu <3233990645@qq.com>
2021-11-13 15:40:33 +0800

    取消在爬取过程中记录爬过的asin号的部分:
    采用一个新的程序（recordAsin.py）来记录爬取过的asin

---
+++ recordAsin.py
@@ -0,0 +1,16 @@
+import json
+
+import jsonlines
+import pandas as pd
+
+df = pd.read_csv('newAsin.csv')  # 存储是否被爬取的记录
+movies = open("movie.json", encoding='UTF-8')
+i = 0
+for item in jsonlines.Reader(movies):
+    if i == 100:
+        break
+    asin = item['ASIN']
+    print(df.loc[df['asin'] == asin])
+    df.loc[df['asin'] == asin, 'isGot'] = 1
+
+df.to_csv("newAsin.csv", index=False)
