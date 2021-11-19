d6b6528d6c8a78715d6df1beab4e70a84cfb5c06
Haotian Wu <3233990645@qq.com>
2021-11-19 12:22:22 +0800

    加入计数：总数；不是电影的数量

--- recordAsin.py
+++ recordAsin.py
@@ -1,14 +1,21 @@
-import json
-
 import jsonlines
 import pandas as pd
 
 df = pd.read_csv('newAsin.csv')  # 存储是否被爬取的记录
 movies = open("movie.json", encoding='UTF-8')
 
+count = 0
+notMoviecount = 0
 for item in jsonlines.Reader(movies):
     asin = item['ASIN']
+
+    if 'isMovie' in item:
+        notMoviecount += 1
     # print(df.loc[df['asin'] == asin])
     df.loc[df['asin'] == asin, 'isGot'] = 1
 
+    count += 1
+
 df.to_csv("newAsin.csv", index=False)
+print(count)
+print(notMoviecount)
