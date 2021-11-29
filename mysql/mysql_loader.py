2a4457e2778a9dba89ff34a4658fc67bc6beec4e
Wizard23333 <3233990645@qq.com>
2021-11-29 21:48:15 +0800

    加入判断电影系列部分

--- mysql/mysql_loader.py
+++ mysql/mysql_loader.py
@@ -3,10 +3,44 @@ import math
 
 import jsonlines
 import pymysql
+import textdistance
+
+no_meaning_str = ["The", "A", "Man", "How", "From"]
+
+
+def is_same_series(str1, str2):
+    if textdistance.levenshtein.normalized_similarity(str1[0], str2[0]) < 0.9:
+        return 0
+    else:
+        length = len(str2)
+        if len(str1) < len(str2):
+            length = len(str1)
+        similarity = 0
+
+        # 设置权重
+        for index in range(length):
+            similarity += textdistance.levenshtein.normalized_similarity(str1[index], str2[index]) * (
+                    length - index)
+        similarity /= (0 + length) * length
+        # 含有某些特无意义殊单词降低其相似度
+        if str1[0] in no_meaning_str:
+            similarity *= 0.8
+        # 降低某些短标题的出现概率
+        if length < 6:
+            similarity *= length / 6
+        # 降低相差较大的出现概率
+        similarity *= 1 - (abs(len(str1) - len(str2)) / (len(str1) + len(str2)))
+        if similarity > 0.29:
+            return similarity
+            print(str1, str2)
+            print(similarity)
+        else:
+            return 0
+
 
 # 数据库连接
 dw_mysqldb = pymysql.connect(
-    host='139.196.202.57',
+    host='127.0.0.1',
     user='dataWarehouseAdm',
     password='dataWarehouse_123',
     database='dataWarehouseDB',
@@ -14,18 +48,6 @@ dw_mysqldb = pymysql.connect(
 
 # 可以执行sql的光标对象
 cursor = dw_mysqldb.cursor()
-
-# 测试用例
-# sql = "INSERT INTO div_time(year) VALUES (%s);"
-# time_key = 10;
-# year = 2021;
-#
-# try:
-#     cursor.execute(sql, [year])
-#     dw_mysqldb.commit()
-# except Exception as e:
-#     print(e)
-#     dw_mysqldb.rollback()
 
 MONTHS = ["January", "February", "March", "April", "May", "June", "July",
           "August", "September", "October", "November", "December", ]
@@ -53,7 +75,7 @@ for item in jsonlines.Reader(movies):
             continue
         # 可能存在同一个商品的不同页面情况，这种情况需要跳过
         if len(item['reviews']) != 0:
-            cursor.excute(sql_select_review_id, [item['reviews'][0]['reviewerid']])
+            cursor.execute(sql_select_review_id, [item['reviews'][0]['reviewerid']])
             if cursor.rowcount != 0:
                 continue
     except Exception as e:
@@ -126,22 +148,58 @@ for item in jsonlines.Reader(movies):
         dw_mysqldb.rollback()
 
     # div_movie_series
-    # 暂时还未进行模糊匹配，单纯将电影名当成一个series
+    # # 暂时还未进行模糊匹配，单纯将电影名当成一个series
+    # movie_series_id = None
+    # sql_select_unique_movie_series = "SELECT movie_series_id FROM div_movie_series WHERE series_name = %s;"
+    # sql_insert_movie_series = "INSERT INTO div_movie_series(series_name, series_num)" \
+    #                           "VALUES (%s, %s);"
+    # sql_update_movie_series = "UPDATE div_movie_series SET series_num = series_num + 1 WHERE series_name = %s;"
+    # # 执行
+    # try:
+    #     cursor.execute(sql_select_unique_movie_series, [item['Title']])
+    #     if cursor.rowcount == 0:
+    #         cursor.execute(sql_insert_movie_series, [item['Title'], 1])
+    #         dw_mysqldb.commit()
+    #     else:
+    #         cursor.execute(sql_update_movie_series, [item['Title']])
+    #         dw_mysqldb.commit()
+    #     cursor.execute(sql_select_unique_movie_series, [item['Title']])
+    #     movie_series_id = cursor.fetchone()[0]
+    #     # print("&", movie_series_id)
+    # except Exception as e:
+    #     print(e)
+    #     dw_mysqldb.rollback()
+
+    # div_movie_series
     movie_series_id = None
     sql_select_unique_movie_series = "SELECT movie_series_id FROM div_movie_series WHERE series_name = %s;"
+    sql_select_all_movie_series = "SELECT series_name FROM div_movie_series;"
     sql_insert_movie_series = "INSERT INTO div_movie_series(series_name, series_num)" \
                               "VALUES (%s, %s);"
     sql_update_movie_series = "UPDATE div_movie_series SET series_num = series_num + 1 WHERE series_name = %s;"
-    # 执行
     try:
-        cursor.execute(sql_select_unique_movie_series, [item['Title']])
-        if cursor.rowcount == 0:
-            cursor.execute(sql_insert_movie_series, [item['Title'], 1])
+        cursor.execute(sql_select_all_movie_series)  # 选取所有的系列名称
+        movie_series_names = cursor.fetchall()
+        series_name = None
+        series_name_similarity = 0
+        for name in movie_series_names:
+            # 如果当前的电影名称和某一个系列名称相似 找到最大的相似度
+            if is_same_series(name, item['Title']) > series_name_similarity:
+                print(is_same_series(name, item['Title']))
+                series_name_similarity = is_same_series(name, item['Title'])
+                series_name = name  # 设置当前系列的名称
+        print(item['Title'], series_name)
+        # 如果都没有相似的，自己即成为一个系列
+        if series_name_similarity == 0:
+            series_name = item['Title']
+            cursor.execute(sql_insert_movie_series, [series_name, 1])
             dw_mysqldb.commit()
+        # 有相似的就更新
         else:
-            cursor.execute(sql_update_movie_series, [item['Title']])
+            cursor.execute(sql_update_movie_series, [series_name])
             dw_mysqldb.commit()
-        cursor.execute(sql_select_unique_movie_series, [item['Title']])
+        # 查询到当前的series_id
+        cursor.execute(sql_select_unique_movie_series, series_name)
         movie_series_id = cursor.fetchone()[0]
         # print("&", movie_series_id)
     except Exception as e:
@@ -161,13 +219,14 @@ for item in jsonlines.Reader(movies):
         print("style_key error")
     directors_name = ','.join(item['Director'])
     actors_name = ','.join(item['Actors'])
+    review_point = item['ReviewPoint']
 
     sql_insert_movie = "INSERT INTO fact_movie(asin, movie_series_id, title, product_version, imdb_score, " \
-                       "time_key, style_key, directors_name, actors_name)" \
-                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+                       "time_key, style_key, directors_name, actors_name, review_point)" \
+                       "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
     try:
         cursor.execute(sql_insert_movie, [asin, movie_series_id, title, product_version, imdb_score,
-                                          time_key, style_key, directors_name, actors_name])
+                                          time_key, style_key, directors_name, actors_name, review_point])
         dw_mysqldb.commit()
     except Exception as e:
         print(e)
