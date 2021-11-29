b3ddd1ced7ee87545f9ee4f9c65592dbf4836013
zb2313 <2224105295@qq.com>
2021-11-29 10:44:17 +0800

    Update movies_edit.py

--- movies_edit.py
+++ movies_edit.py
@@ -1,11 +1,19 @@
 import jsonlines
 import json
 
-file_path = 'E:\\movies.json'
 file = open('E:\\movies.json', 'r', encoding='utf-8')
 file2 = jsonlines.open('E:\\movies2.json', 'a')
 for line in file.readlines():
     dic = json.loads(line)
+    # 评论得分处理 存在没评论的情况
+    if len(dic['reviews']) >= 1:
+        t = 0
+        for item in dic['reviews']:
+            t += float(item['score'])
+        average = round(t / len(dic['reviews']), 1)
+        print(average)
+        dic['ReviewPoint'] = average
+    # 时间处理
     dic_split = str(dic['Date First Available']).replace(' ', ',').split(',')
     if len(dic_split) != 3 and len(dic['reviews']) >= 1:  # 取评论
         print(dic['reviews'][0]['date'])
@@ -19,7 +27,9 @@ for line in file.readlines():
     date = str(dic['Date First Available']).replace(',', '').split(' ')
     print(date)
     dic['Date First Available'] = date
+
     file2.write(dic)
+
 file.close()
 file2.close()
 
