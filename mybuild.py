3df7109924a93387bfa82e3e3ee94c77f69c6e72
Sry-maker <1961384758@qq.com>
2021-12-12 10:28:09 +0800

    更新图数据库存储代码（纯纯折磨）

--- mybuild.py
+++ mybuild.py
@@ -14,7 +14,7 @@ def remove_query(query):
 class MedicalGraph:
     def __init__(self):
         cur_dir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
-        self.data_path = os.path.join(cur_dir, 'movies3.json')
+        self.data_path = os.path.join(cur_dir, 'data/movies3.json')
         self.g = Graph("http://47.103.9.250:7474/", auth=("neo4j", "test"))
 
     '''读取文件'''
@@ -130,8 +130,8 @@ class MedicalGraph:
                 if len(tempdir) == 2:
                     dir1 = remove_query(tempdir[0])
                     dir2 = remove_query(tempdir[1])
-                    cooperate_aanda.append([dir1, dir2])
-                    cooperate_aanda.append([dir2, dir1])
+                    cooperate_dandd.append([dir1, dir2])
+                    cooperate_dandd.append([dir2, dir1])
                 for director in data_json['Director']:
                     if 'Actors' in data_json:
                         for actor in data_json['Actors']:
@@ -155,7 +155,7 @@ class MedicalGraph:
         all_times = []
         for tim in times:
             if tim not in all_times:
-                all_times.append(remove_query(tim))
+                all_times.append(tim)
         return set(movies), set(actors), set(directors), set(styles), set(versions), movie_infos, all_times, \
                belongs_to, do_act, do_dir, cooperate_aandd, cooperate_danda, cooperate_aanda, cooperate_dandd, has_version, has_time
 
@@ -203,18 +203,67 @@ class MedicalGraph:
 
     def create_graphrels(self):
         movies, actors, directors, styles, versions, movie_infos, times, belongs_to, do_act, do_dir, cooperate_aandd, cooperate_danda, cooperate_aanda, cooperate_dandd, has_version, has_time = self.read_nodes()
-        self.create_relationship('Movie', 'Style', belongs_to, 'belongs_to', '电影的类型')
-        self.create_relationship('Movie', 'Actor', do_act, 'do_act', '演员演电影')
-        self.create_relationship('Movie', 'Director', do_dir, 'do_dir', '导演导电影')
-        self.create_relationship('Actor', 'Director', cooperate_aandd, 'cooperate_aandd', '演员和导演合作')
-        self.create_relationship('Director', 'Actor', cooperate_danda, 'cooperate_danda', '导演和演员合作')
-        self.create_relationship('Actor', 'Actor', cooperate_aanda, 'cooperate_aanda', '演员和演员合作')
-        self.create_relationship('Director', 'Director', cooperate_dandd, 'cooperate_dandd', '导演和导演合作')
-        self.create_relationship('Movie', 'Version', has_version, 'has_version', '电影的版本')
-        self.create_relationship('Movie', 'Time', has_time, 'has_time', '电影的上映时间')
+        # self.create_relationship('Movie', 'Style', belongs_to, 'belongs_to', '电影的类型')
+        # self.create_relationship('Movie', 'Actor', do_act, 'do_act', '演员演电影')
+        # self.create_relationship('Movie', 'Director', do_dir, 'do_dir', '导演导电影')
+        # self.create_relationship('Actor', 'Director', cooperate_aandd, 'cooperate_aandd', '演员和导演合作')
+        # self.create_relationship('Director', 'Actor', cooperate_danda, 'cooperate_danda', '导演和演员合作')
+        # self.create_relationship('Actor', 'Actor', cooperate_aanda, 'cooperate_aanda', '演员和演员合作')
+        # self.create_relationship('Director', 'Director', cooperate_dandd, 'cooperate_dandd', '导演和导演合作')
+        # self.create_relationship('Movie', 'Version', has_version, 'has_version', '电影的版本')
+        self.create_relationshiptime('Movie', 'Time', has_time, 'has_time', '电影的上映时间')
 
     '''创建实体关联边'''
 
+    def create_relationshiptime(self, start_node, end_node, edges, rel_type, rel_name):
+        count = 0
+        # 去重处理
+        # set_edges = []
+        # for edge in edges:
+        #     set_edges.append('###'.join(edge))
+        all = len((edges))
+        # sall=len(set_edges)
+        for edge in edges:
+            # edge = edge.split('###')
+            p = edge[0]
+            q = edge[1]
+            if len(q)!=3:
+                continue;
+            # match(p:Movie{name:"TEST0"}),(q:Style{name:"General"}),z=(p)-[r:belongs_to]->(q) RETURN count(z)   match(p:Movie{name:"TEST0"}),(q:Style{name:"General"}),z=(p)-[r:belongs_to]->(q)
+            # set r.num=r.num+1
+
+            # match(p:Movie{name:"TEST1"}),(q:Style{name:"General"}),z=(p)-[r:belongs_to]->(q)
+            # return r.num
+            verify = 'match(p:%s{name:"%s"}),(q:%s{name:["%s","%s","%s"]}),z=(p)-[r:%s]->(q) RETURN r.num' % (
+            start_node, p, end_node, q[0],q[1],q[2], rel_type)
+            r_num = self.g.run(verify)
+            temp_num0 = r_num.data()
+            if len(temp_num0) == 0:
+                query = 'match(p:%s),(q:%s) where p.name="%s" and q.name=["%s","%s","%s"] create (p)-[rel:%s{name:"%s",num:%d}]->(q)' % (
+                start_node, end_node, p, q[0],q[1],q[2], rel_type, rel_name, 1)
+            else:
+                query = 'match(p:%s{name:"%s"}),(q:%s{name:["%s","%s","%s"]}),z=(p)-[r:%s]->(q) set r.num=r.num+1' % (
+                start_node, p, end_node, q[0],q[1],q[2], rel_type)
+            # temp_num1=r_num.data().pop()
+            # num=list(temp_num1.values())[0]
+            #
+            # if num==0:
+            #     query = 'match(p:%s),(q:%s) where p.name="%s" and q.name="%s" create (p)-[rel:%s{name:"%s",num:%d}]->(q)' % (start_node, end_node, p, q, rel_type, rel_name, 1)
+            #     #match(p:Movie{name:"TEST0"}),(q:Style{name:"General"}),z=(p)-[r:belongs_to]->(q) RETURN z.num
+            # if num!=0:
+            #     query='match(p:%s{name:"%s"}),(q:%s{name:"%s"}),z=(p)-[r:%s]->(q) set r.num=r.num+1' %(start_node,p,end_node,q,rel_type)
+            # 这里高斯劳资了，西巴 MATCH (<node1-label-name>:<nade1-name>),(<node2-label-name>:<node2-name>)
+            # CREATE
+            #   (<node1-label-name>)-[<relationship-label-name>:<relationship-name>{<define-properties-list}]
+            # query = 'match(p:%s),(q:%s) where p.name="%s" and q.name="%s" create (p)-[rel:%s{name:"%s",num:%d}]->(q)' % (start_node, end_node, p, q, rel_type, rel_name, 1)
+            # query = "match(p:" + start_node + "),(q:" + end_node + ") where p.name='" + p + "' and q.name='" + q + "' create (p)-[rel:" + rel_type + "{name:'" + rel_name + "'}]->(q)"
+            try:
+                self.g.run(query)
+                count += 1
+                print(rel_type, count, all)
+            except Exception as e:
+                print(e)
+        return
     def create_relationship(self, start_node, end_node, edges, rel_type, rel_name):
         count = 0
         # 去重处理
@@ -266,7 +315,8 @@ class MedicalGraph:
 
 if __name__ == '__main__':
     handler = MedicalGraph()
-    print("step1:导入图谱节点中")
-    handler.create_graphnodes()
+    # print("step1:导入图谱节点中")
+    # handler.create_graphnodes()
     print("step2:导入图谱边中")
     handler.create_graphrels()
+    print("本次操作结束")
